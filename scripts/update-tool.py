99618acfbd21bf9f5818b4d8324a9fbcd0bdea10
Matthias Bernt <m.bernt@ufz.de>
2024-06-17 15:42:57 +0200

    fixup

--- scripts/update-tool.py
+++ scripts/update-tool.py
@@ -12,7 +12,7 @@ ts = toolshed.ToolShedInstance(url="https://toolshed.g2.bx.psu.edu")
 
 
 def update_file(fn, owner=None, name=None, without=False):
-    success = true
+    success = True
     with open(fn + ".lock", "r") as handle:
         locked = yaml.safe_load(handle)
 
